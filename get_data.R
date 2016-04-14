b7fa471edb1ae03a058b21544217e7e7e726d6cc
sndekoning <sylvester.de.koning90@gmail.com>
2016-04-14 15:26:55 +0200

    Initial commit

---
+++ get_data.R
@@ -0,0 +1,13 @@
+# Checking for and if needed creating the data folder
+if(!dir.exists("./data")){
+    dir.create("./data")
+}    
+
+# Checking for and if needed downloading the data,unzipping 548 MB
+if(!file.exists("./data/Coursera_Swiftkey.zip")){
+
+   get_url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+   download.file(get_url, "./data/Coursera_Swiftkey.zip")
+   unzip("./data/Coursera_Swiftkey.zip", exdir = "./data")
+}
+
