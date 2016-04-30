5297e6ec52dbad30fc1f835bee04aaf2feba50cb
Sylvester de Koning <sylvester.de.koning90@gmail.com>
2016-04-30 15:14:28 +0200

    added profanity filter and markdown report skeleton.

--- text_mining.R
+++ text_mining.R
@@ -9,25 +9,30 @@ if(!file.exists("./data/final/en_US/blog_lines_sample.RData")){
     load("./data/final/en_US/twitter_lines_sample.RData")
 }
 
-# Loading dependencies
+# Loading dependencies.
 library(tm)
 library(dplyr)
 
 total_sample <- paste(blog_lines_sample, news_lines_sample, twitter_lines_sample)
-doc_corpus <- VCorpus(VectorSource(total_sample))
+doc_corpus <- Corpus(VectorSource(total_sample), readerControl = list(reader=readPlain, language="en_US", load=TRUE))
 
 # Getting bad words.
-bw_url <- "https://github.com/shutterstock/List-of-Dirty-Naughty-Obscecne-and-Otherwise-Bad-Words/blob/master/en"
-download.file(bw_url, "./bad_words.txt")
 
-doc_copus_clean <- doc_corpus %>% 
-    tm_map(stripWhitespace) %>%
-    tm_map(content_transformer(tolower)) %>%
+bad_words <- scan("./bad_words.txt", what = "character", sep = "\n")
+
+# Cleaning up the corpus.
+remove_url <- function(x) {
+    gsub("http[[:alnum:]]*", "", x)
+    }
+
+doc_corpus_clean <- doc_corpus %>% 
     tm_map(content_transformer(removePunctuation)) %>%
     tm_map(content_transformer(removeNumbers)) %>%
-    tm_map(removeWords, stopwords("english")) %>%
-    
+    tm_map(content_transformer(remove_url)) %>%
+    # tm_map(stripWhitespace) %>%
+    # tm_map(removeWords, stopwords("english")) %>%
+    # tm_map(removeWords, bad_words) %>%
+    # tm_map(stemDocument) %>%
+    # tm_map(stripWhitespace) 
 
-doc_term_matrix <- TermDocumentMatrix(doc_corpus)
-
-mx <- as.matrix(doc_term_matrix)
+doc_term_matrix <- TermDocumentMatrix(doc_corpus_clean)
