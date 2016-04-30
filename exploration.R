37f569cdfee7bc46b22e5555a708a9777a89f06c
sndekoning <sylvester.de.koning90@gmail.com>
2016-04-30 22:28:03 +0200

    added frequency plot

--- exploration.R
+++ exploration.R
@@ -11,3 +11,14 @@ if(!file.exists("./data/sparse_dtm.RData")){
 
 # Getting Word Frequencies.
 word_frequencies <- sort(colSums(as.matrix(sparse_dtm)), decreasing = TRUE)
+word_frequencies_df <- data.frame(word = names(word_frequencies), frequency = word_frequencies)
+
+# Frequency plot.
+library(ggplot2)
+
+ggplot(word_frequencies_df[1:50, ,], aes(x=word, y=frequency)) +
+    geom_bar(stat="identity") +
+    xlab("") +
+    ylab("Frequency") +
+    ggtitle("Top 50 most frequently occurring words over the three datasets") + 
+    coord_flip()
