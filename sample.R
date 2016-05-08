39ead5a5630fe8bbd1da9f9f7f8bbfc0523d6908
Sylvester de Koning <sylvester.de.koning90@gmail.com>
2016-05-08 11:05:00 +0200

    Initial commit for prediction.R, switched up functionality from sampling and mining scripts.

--- sample.R
+++ sample.R
@@ -1,11 +1,9 @@
 source("get_data.R")
 
-# Creating smaller workable samples from the 3 data-sets.
-setwd("./data/final/en_US")
-
+# === Creating smaller workable samples from the 3 data-sets. ===
 
 # Sampling 5% of the lines of the twitter file.
-file_name <- "en_US.twitter.txt"
+file_name <- "./data/final/en_US/en_US.twitter.txt"
 con <- file(file_name, open = "r")
 twitter_lines <- readLines(con, encoding = "UTF-8")
 close(con)
@@ -14,12 +12,12 @@ set.seed(290420161)
 
 twitter_lines_sample <- sample(twitter_lines, size = length(twitter_lines)*.05)
 
-save(twitter_lines_sample, file = "twitter_lines_sample.RData")
-rm(list = ls())
+save(twitter_lines_sample, file = "./data/twitter_lines_sample.RData")
+rm(twitter_lines)
 gc(verbose = FALSE)
 
 # Sampling 5% of the lines in the news file.
-file_name <- "en_US.news.txt"
+file_name <- "./data/final/en_US/en_US.news.txt"
 con <- file(file_name, open = "r")
 news_lines <- readLines(con, encoding = "UTF-8")
 close(con)
@@ -28,12 +26,12 @@ set.seed(290420162)
 
 news_lines_sample <- sample(news_lines, size = length(news_lines)*.05)
 
-save(news_lines_sample, file = "news_lines_sample.RData")
-rm(list = ls())
+save(news_lines_sample, file = "./data/news_lines_sample.RData")
+rm(news_lines)
 gc(verbose = FALSE)
 
 # Sampling 5% of the lines in the blog file.
-file_name <- "en_US.blogs.txt"
+file_name <- "./data/final/en_US/en_US.blogs.txt"
 con <- file(file_name, open = "r")
 blog_lines <- readLines(con, encoding = "UTF-8")
 close(con)
@@ -42,10 +40,14 @@ set.seed(290420163)
 
 blog_lines_sample <- sample(blog_lines, size = length(blog_lines)*.05)
 
-save(blog_lines_sample, file = "blog_lines_sample.RData")
-rm(list = ls())
+save(blog_lines_sample, file = "./data/blog_lines_sample.RData")
+rm(blog_lines)
 gc(verbose = FALSE)
 
-setwd("..")
-setwd("..")
-setwd("..")
+# Combining into total sample.
+
+total_sample <- paste(blog_lines_sample, news_lines_sample, twitter_lines_sample)
+rm(blog_lines_sample, news_lines_sample, twitter_lines_sample)
+total_sample <- iconv(total_sample, from = "UTF-8", to = "latin1", sub = "")
+
+save(total_sample, file = "./data/total_sample.RData")
--- text_mining.R
+++ text_mining.R
@@ -1,12 +1,10 @@
 # Retrieving the sampled data.
-if(!file.exists("./data/final/en_US/blog_lines_sample.RData")){
+if(!file.exists("./data/total_lines_sample.RData")){
     print("Sample data not present, sampling data...")
     source("sample.R")
 } else{
     print("Sample data present, getting data...")
-    load("./data/final/en_US/blog_lines_sample.RData")
-    load("./data/final/en_US/news_lines_sample.RData")
-    load("./data/final/en_US/twitter_lines_sample.RData")
+    load("./data/total_lines_sample.RData")
 }
 
 # Loading dependencies.
@@ -15,10 +13,7 @@ library(dplyr)
 library(SnowballC)
 library(RWeka)
 
-total_sample <- paste(blog_lines_sample, news_lines_sample, twitter_lines_sample)
-rm(blog_lines_sample, news_lines_sample, twitter_lines_sample)
-total_sample <- iconv(total_sample, from = "UTF-8", to = "latin1", sub = "")
-
+#
 doc_corpus <- Corpus(VectorSource(total_sample))
 
 # Getting bad words.
